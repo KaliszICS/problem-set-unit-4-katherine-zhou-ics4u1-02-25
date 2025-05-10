a3e02cc9d8955de54b730ec08ef63bacfc714c88
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-05-10 03:52:21 +0000

    Update Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -94,5 +94,133 @@ public class Player{
         this.hand = newHand;
     }
 
+    /**
+     * Discards a card from the hand to the given deck's discard pile
+     * Returns true if the card exists in hand, false otherwise
+     * @param card is the card that will be discarded
+     * @param discardPile is the pile the card will be discarded into
+     * @return returns true if the card exists and was discarded, false otherwise
+     */
+    public boolean discardCard(Card card, DiscardPile discardPile){
+        if (card == null || this.hand.length == 0){
+            return false;
+        }
+        for (int i = 0; i < this.hand.length; i++){
+            if (this.hand[i].equals(card)){
+                discardPile.addCard(card);
+                Card[] newHand = new Card[this.hand.length - 1];
+                for (int a = 0, b = 0; a < this.hand.length; a++){
+                    if (a != i){
+                        newHand[b++] = this.hand[a];
+                    }
+                }
+                this.hand = newHand;
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Returns the specified card to the deck
+     * Returns true if the card exists in hand, false otherwise
+     * @param card is the card that will be returned to the deck
+     * @param deck is the deck the card will be returned to
+     * @return returns true if the card exists and was returned, false otherwise
+     */
+    public boolean returnCard(Card card, Deck deck){
+        //null cases
+        if (card == null || this.hand.length == 0){
+            return false;
+        }
+        for (int i = 0; i < this.hand.length; i++){
+            if (this.hand[i].equals(card)){
+                deck.addCard(card);
+                Card[] newHand = new Card[this.hand.length - 1];
+                for (int a = 0, b = 0; a < this.hand.length; a++){
+                    if (a != i){
+                        newHand[b++] = this.hand[a];
+                    }
+                }
+                this.hand = newHand;
+                return true;
+            }
+        }
+        return false;
+    }
 
+    /**
+     * Will print out their name, age, and hand of cards
+     * @return returns the player's name, age, and hand of cards (eg. "Mr. Kalisz, 99, Ace of hearts, King of Hearts, Queen of Hearts, Jack of Hearts, 9 of Spades")
+     */
+    public String toString(){
+        String person = this.name + ", " + this.age;
+        if (this.hand.length == 0){
+            return person;
+        }
+        for (int i = 0; i < this.hand.length; i++){
+            person += ", " + this.hand[i].toString();
+        }
+        return person;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
